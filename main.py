852bfcefb49e63f18b34eca1efb561afbc9944c7
2black0 <2black0@gmail.com>
2024-07-24 22:10:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
-# This is a test file of the OTA code
+import network
+import time
+import machine
+from umqtt.simple import MQTTClient
 
-print('Hello World')
+# Koneksi ke broker MQTT
+mqtt_server = 'broker.emqx.io'
+client_id = 'esp32_client'
+topic_pub_temp = b'esp32/sensor/temp'
+topic_pub_hum = b'esp32/sensor/hum'
+topic_sub = b'esp32/reset'
 
-print('This is the update from OTA')
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if msg == b'reset':
+        print('Reset command received, resetting...')
+        machine.reset()
 
-print('This is second update from OTA')
+client = MQTTClient(client_id, mqtt_server)
+client.set_callback(sub_cb)
+client.connect()
+client.subscribe(topic_sub)
+
+print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+
+def publish():
+    while True:
+        temp_msg = b'25.5'  # Example temperature value
+        hum_msg = b'60'     # Example humidity value
+        client.publish(topic_pub_temp, temp_msg)
+        client.publish(topic_pub_hum, hum_msg)
+        print('Published temperature: %s' % temp_msg)
+        print('Published humidity: %s' % hum_msg)
+        time.sleep(10)  # Publish every 10 seconds
+
+def subscribe():
+    while True:
+        client.check_msg()
+        time.sleep(1)
+
+# Main loop
+try:
+    import _thread
+    _thread.start_new_thread(publish, ())
+    subscribe()
+except KeyboardInterrupt:
+    print('Disconnected from broker')
+    client.disconnect()
