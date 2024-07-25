f2fce930a2ddb42aa5fc59ff1d71ee8f72141a3c
2black0 <2black0@gmail.com>
2024-07-25 11:36:53 +0700

    update

--- main.py
+++ main.py
@@ -1,48 +1,87 @@
-import network
+from machine import Pin, reset
 import time
-import machine
+import random
 from umqtt.simple import MQTTClient
+import json
 
-# Koneksi ke broker MQTT
-mqtt_server = 'broker.emqx.io'
+# Tentukan pin GPIO yang digunakan untuk LED
+led = Pin(2, Pin.OUT)  # Pin 2 sering digunakan sebagai LED bawaan pada ESP32
+
+# Informasi MQTT
+mqtt_server = '172.16.0.35'
+mqtt_topic_reset = 'esp32/reset'
+mqtt_topic_temp = 'esp32/sensor/temp'
+mqtt_topic_hum = 'esp32/sensor/hum'
+mqtt_topic_led = 'esp32/led'
+mqtt_topic_version = 'esp32/version'
 client_id = 'esp32_client'
-topic_pub_temp = b'esp32/sensor/temp'
-topic_pub_hum = b'esp32/sensor/hum'
-topic_sub = b'esp32/reset'
 
-def sub_cb(topic, msg):
+from ota import OTAUpdater
+
+#firmware_url = "https://qrcode.beais-uny.id/firmware/"
+firmware_url = "https://raw.githubusercontent.com/2black0/ota-esp32/"
+SSID = "Fave"
+PASSWORD = "freewifi"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.mpy")
+ota_updater.download_and_install_update_if_available()
+
+# Fungsi callback ketika menerima pesan dari broker MQTT
+def mqtt_callback(topic, msg):
     print((topic, msg))
-    if msg == b'reset':
-        print('Reset command received, resetting...')
-        machine.reset()
+    if topic.decode() == mqtt_topic_reset and msg.decode() == 'reset':
+        reset()
+    elif topic.decode() == mqtt_topic_led:
+        if msg.decode() == 'true':
+            led.value(1)  # Nyalakan LED
+        elif msg.decode() == 'false':
+            led.value(0)  # Matikan LED
 
-client = MQTTClient(client_id, mqtt_server)
-client.set_callback(sub_cb)
-client.connect()
-client.subscribe(topic_sub)
+# Fungsi untuk menghubungkan ke broker MQTT
+def connect_mqtt():
+    client = MQTTClient(client_id, mqtt_server)
+    client.set_callback(mqtt_callback)
+    client.connect()
+    client.subscribe(mqtt_topic_reset)
+    client.subscribe(mqtt_topic_led)
+    print('Connected to MQTT broker and subscribed to topics:', mqtt_topic_reset, mqtt_topic_led)
+    return client
 
-print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
+# Fungsi untuk membaca versi dari file JSON
+def read_version():
+    try:
+        with open('version.json', 'r') as f:
+            data = json.load(f)
+            return data.get('version', None)
+    except Exception as e:
+        print('Failed to read version:', e)
+        return None
 
-def publish():
-    while True:
-        temp_msg = b'25.5'  # Example temperature value
-        hum_msg = b'60'     # Example humidity value
-        client.publish(topic_pub_temp, temp_msg)
-        client.publish(topic_pub_hum, hum_msg)
-        print('Published temperature: %s' % temp_msg)
-        print('Published humidity: %s' % hum_msg)
-        time.sleep(10)  # Publish every 10 seconds
+# Membaca versi dari file version.json
+version = read_version()
 
-def subscribe():
-    while True:
-        client.check_msg()
-        time.sleep(1)
+# Menghubungkan ke broker MQTT
+client = connect_mqtt()
 
-# Main loop
-try:
-    import _thread
-    _thread.start_new_thread(publish, ())
-    subscribe()
-except KeyboardInterrupt:
-    print('Disconnected from broker')
-    client.disconnect()
+# Mengirim versi ke topik esp32/version
+if version is not None:
+    client.publish(mqtt_topic_version, str(version))
+    print(f'Published version: {version} to {mqtt_topic_version}')
+
+# Loop utama
+last_publish_time = time.time()
+
+while True:
+    client.check_msg()  # Memeriksa pesan yang masuk
+    time.sleep(0.1)  # Tunggu selama 0.1 detik
+
+    # Memeriksa apakah 5 detik telah berlalu untuk publish nilai sensor
+    current_time = time.time()
+    if current_time - last_publish_time >= 5:
+        temp = random.randint(0, 100)
+        hum = random.randint(0, 100)
+        client.publish(mqtt_topic_temp, str(temp))
+        client.publish(mqtt_topic_hum, str(hum))
+        print(f'Published temp: {temp} to {mqtt_topic_temp}')
+        print(f'Published hum: {hum} to {mqtt_topic_hum}')
+        last_publish_time = current_time
