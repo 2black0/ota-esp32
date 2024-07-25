166417d7eaced2a383745ccb40670db266a533b1
2black0 <2black0@gmail.com>
2024-07-25 13:35:22 +0700

    update

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from machine import Pin, reset
+from machine import Pin, reset, time_pulse_us
 import time
 import random
 from umqtt.simple import MQTTClient
 import json
-#import dht  # Pastikan pustaka DHT sudah diinstal
+import dht  # Pastikan pustaka DHT sudah diinstal
 
 # Tentukan pin GPIO yang digunakan untuk LED
 led = Pin(2, Pin.OUT)  # Pin 2 sering digunakan sebagai LED bawaan pada ESP32
-#dht_sensor = dht.DHT11(Pin(14))  # Sensor DHT11 pada pin 23
+dht_sensor = dht.DHT11(Pin(23))  # Sensor DHT11 pada pin 23
+
+# Tentukan pin untuk sensor SRF04
+trigger = Pin(18, Pin.OUT)
+echo = Pin(19, Pin.IN)
 
 # Informasi MQTT
 mqtt_server = '172.16.0.35'
@@ -16,6 +20,7 @@ mqtt_topic_temp = 'esp32/sensor/temp'
 mqtt_topic_hum = 'esp32/sensor/hum'
 mqtt_topic_led = 'esp32/led'
 mqtt_topic_version = 'esp32/version'
+mqtt_topic_distance = 'esp32/sensor/distance'
 client_id = 'esp32_client'
 
 from ota import OTAUpdater
@@ -58,6 +63,21 @@ def read_version():
         print('Failed to read version:', e)
         return None
 
+# Fungsi untuk mengukur jarak dengan sensor SRF04
+def measure_distance():
+    # Kirim sinyal trigger
+    trigger.off()
+    time.sleep_us(2)
+    trigger.on()
+    time.sleep_us(10)
+    trigger.off()
+    
+    # Tunggu echo dan hitung durasi pulsa
+    duration = time_pulse_us(echo, 1, 30000)  # Timeout 30ms
+    distance = (duration / 2) / 29.1  # Konversi durasi ke jarak dalam cm
+    
+    return distance
+
 # Membaca versi dari file version.json
 version = read_version()
 
@@ -79,13 +99,19 @@ while True:
     # Memeriksa apakah 5 detik telah berlalu untuk publish nilai sensor
     current_time = time.time()
     if current_time - last_publish_time >= 5:
-        #dht_sensor.measure()
-        #temp = dht_sensor.temperature()
-        #hum = dht_sensor.humidity()
-        temp = random.randint(0, 100)
-        hum = random.randint(0, 100)
+        # Membaca nilai dari sensor DHT11
+        dht_sensor.measure()
+        temp = dht_sensor.temperature()
+        hum = dht_sensor.humidity()
+        
+        # Mengukur jarak dengan sensor SRF04
+        distance = measure_distance()
+
+        # Publish nilai sensor ke MQTT
         client.publish(mqtt_topic_temp, str(temp))
         client.publish(mqtt_topic_hum, str(hum))
+        client.publish(mqtt_topic_distance, str(distance))
         print(f'Published temp: {temp} to {mqtt_topic_temp}')
         print(f'Published hum: {hum} to {mqtt_topic_hum}')
+        print(f'Published distance: {distance} to {mqtt_topic_distance}')
         last_publish_time = current_time
